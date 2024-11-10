b784c9fdb4ff8ba882daada564d2e916d3247958
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2024-11-10 12:09:51 -0600

    updating email to copy - doesn't work though

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+const button = document.querySelector("emailButton");
+button.addEventListener("click", function() {
+    const copyText = document.getElementById("myEmail");
+    copyText.select();
+    copyText.selectSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("copied!")
+});
+// function copyEmail() {
+//     const copyText = document.getElementById("myEmail");
+//     copyText.select();
+//     copyText.selectSelectionRange(0, 99999);
+//     navigator.clipboard.writeText(copyText.value);
+//     alert("copied!")
+// }
